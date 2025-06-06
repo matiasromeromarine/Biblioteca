aad832ea73fccf1ab4099912ab98360ceba87cc0
matiasromeromarine <a01708802@tec.mx>
2025-06-05 20:38:19 -0600

    Update Biblioteca.h

--- Biblioteca.h
+++ Biblioteca.h
@@ -2,21 +2,21 @@
 #define BIBLIOTECA_H
 
 #include "RecursoDigital.h"
-#include <vector>
 #include <string>
 using namespace std;
 
+const int MAX_RECURSOS = 100;
 class Biblioteca {
 private:
-    vector<RecursoDigital*> recursos;
+    RecursoDigital* recursos[MAX_RECURSOS];
     int total_recursos;
 public:
     Biblioteca();
     void agregar_libro(int id, string titulo, int paginas, string autor);
     void agregar_pelicula(int id, string titulo, int duracion_min, string director);
     void agregar_audiolibro(int id, string titulo, int duracion_min, string narrador);
-    vector<RecursoDigital*> buscar_por_titulo(string titulo);
-    vector<RecursoDigital*> recomendar_por_tipo(string tipo);
+    int buscar_por_titulo(string titulo, RecursoDigital* resultados[]);
+    int recomendar_por_tipo(string tipo, RecursoDigital* resultados[]);
     RecursoDigital* recomendar_mejor_calificado();
     RecursoDigital* buscar_por_id(int id);
     ~Biblioteca();

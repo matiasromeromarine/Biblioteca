defa20d845899a769d619f26fdf52eab94d21db9
matiasromeromarine <a01708802@tec.mx>
2025-06-12 22:35:18 -0600

    Update Libro.h

--- Libro.h
+++ Libro.h
@@ -1,17 +1,26 @@
+/*
+ * Proyecto Biblioteca clase "Libro"
+ * Matías Romero Mariné
+ * A01708802
+ * 12/06/2025
+ *
+ * Clase Hija
+ */
 #ifndef LIBRO_H
 #define LIBRO_H
 
 #include "RecursoDigital.h"
 using namespace std;
 
+// Implementación de herencia: la clase hereda de su clase base
 class Libro : public RecursoDigital {
 private:
     int paginas;
     string autor;
 public:
     Libro(int id, string titulo, int paginas, string autor);
-    int calcular_duracion();
-    string mostrar_info();
+    int calcular_duracion() override;
+    string mostrar_info() override;
 };
 
 #endif

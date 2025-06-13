a293669ee1397085b6fa247ee8aea48e0841171a
matiasromeromarine <a01708802@tec.mx>
2025-06-12 22:36:01 -0600

    Update Pelicula.h

--- Pelicula.h
+++ Pelicula.h
@@ -1,17 +1,26 @@
+/*
+ * Proyecto Biblioteca clase "Pelicula"
+ * Matías Romero Mariné
+ * A01708802
+ * 12/06/2025
+ *
+ * Clase hija
+ */
 #ifndef PELICULA_H
 #define PELICULA_H
 
 #include "RecursoDigital.h"
 using namespace std;
 
+// Implementación de herencia: la clase hereda de su clase base
 class Pelicula : public RecursoDigital {
 private:
     int duracion_min;
     string director;
 public:
     Pelicula(int id, string titulo, int duracion_min, string director);
-    int calcular_duracion();
-    string mostrar_info();
+    int calcular_duracion() override;
+    string mostrar_info() override;
 };
 
 #endif

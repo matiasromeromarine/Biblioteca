a2d457ed5c254eb9084f81898183e439a24dca47
matiasromeromarine <a01708802@tec.mx>
2025-06-12 22:37:04 -0600

    Update Usuario.cpp

--- Usuario.cpp
+++ Usuario.cpp
@@ -1,3 +1,9 @@
+/*
+ * Proyecto Biblioteca clase "Usuario"
+ * Matías Romero Mariné
+ * A01708802
+ * 12/06/2025
+ */
 #include "Usuario.h"
 #include "Biblioteca.h"
 using namespace std;
@@ -6,6 +12,7 @@ Usuario::Usuario(string nombre)
     : nombre(nombre) {}
 
 void Usuario::valorar_recurso(Biblioteca& biblioteca, int id, double calif) {
+// Uso de punteros
     RecursoDigital* r = biblioteca.buscar_por_id(id);
     if (r) {
         r->actualizar_clasificacion(calif);

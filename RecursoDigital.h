b19338dd51a8001259bb57fa9cab80048758251f
matiasromeromarine <a01708802@tec.mx>
2025-06-12 22:36:46 -0600

    Update RecursoDigital.h

--- RecursoDigital.h
+++ RecursoDigital.h
@@ -1,3 +1,11 @@
+/*
+ * Proyecto Biblioteca clase "RecursoDigital"
+ * Matías Romero Mariné
+ * A01708802
+ * 12/06/2025
+ *
+ * Clase Abstracta
+ */
 #ifndef RECURSO_DIGITAL_H
 #define RECURSO_DIGITAL_H
 
@@ -12,13 +20,16 @@ protected:
     double clasificacion;
 public:
     RecursoDigital(int id, string titulo, string tipo);
+// Implementación de polimorfismo y clase abstracta con la función virtual pura
     virtual int calcular_duracion() = 0;
+// Implementación de polimorfismo y clase abstracta con la función virtual pura
     virtual string mostrar_info() = 0;
     void actualizar_clasificacion(double nueva);
     int getId();
     string getTitulo();
     string getTipo();
     double getClasificacion();
+// Implementación de polimorfismo con la función pura
     virtual ~RecursoDigital() = default;
 };
 
